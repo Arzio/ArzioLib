9fd6d3f04fa8fd7e6a20482afdb0b99e5993ff90
arzio <arziofox@gmail.com>
2019-03-01 13:47:10 -0300

    Added module to fix plotme denying clicks in corpses/c4/ground items

---
+++ src/com/arzio/arziolib/module/fix/ModuleFixPlotMeEntityInteraction.java
@@ -0,0 +1,38 @@
+package com.arzio.arziolib.module.fix;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import com.arzio.arziolib.ArzioLib;
+import com.arzio.arziolib.api.util.CDEntityType;
+import com.arzio.arziolib.module.ListenerModule;
+
+import fr.xephi.authme.api.API;
+
+public class ModuleFixPlotMeEntityInteraction extends ListenerModule {
+
+	public ModuleFixPlotMeEntityInteraction(ArzioLib plugin) {
+		super(plugin);
+	}
+
+	@Override
+	public String getName() {
+		return "fix-plotme-entity-interaction";
+	}
+
+	@EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
+	public void fixInteraction(PlayerInteractEntityEvent event) {
+		if (API.isAuthenticated(event.getPlayer())) {
+			Entity clickedEntity = event.getRightClicked();
+			
+			if (CDEntityType.GROUND_ITEM.isTypeOf(clickedEntity)
+					|| CDEntityType.C4.isTypeOf(clickedEntity)
+					|| CDEntityType.CORPSE.isTypeOf(clickedEntity)) {
+				event.setCancelled(false);
+			}
+		}
+	}
+
+}
