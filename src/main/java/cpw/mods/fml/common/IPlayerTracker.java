77887672115bbef6509312a9897487a33d4f7d51
arzio <arziofox@gmail.com>
2019-03-05 17:57:18 -0300

    Added clone for IPlayerTracker and Forge's EntityPlayer class

---
+++ src/main/java/cpw/mods/fml/common/IPlayerTracker.java
@@ -0,0 +1,16 @@
+package cpw.mods.fml.common;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * Another tricky way to make Forge classes work fine in Cauldron.
+ */
+public interface IPlayerTracker {
+    void onPlayerLogin(EntityPlayer player);
+
+    void onPlayerLogout(EntityPlayer player);
+
+    void onPlayerChangedDimension(EntityPlayer player);
+
+    void onPlayerRespawn(EntityPlayer player);
+}
