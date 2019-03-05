8f13ef819aef20289980d16438350788989a0087
arzio <arziofox@gmail.com>
2019-03-05 17:58:26 -0300

    Fixed item loss on logout. Fixed item loss when server stops.

--- src/main/java/com/arzio/arziolib/module/fix/ModuleFixCorpseDuplication.java
+++ src/main/java/com/arzio/arziolib/module/fix/ModuleFixCorpseDuplication.java
@@ -4,9 +4,14 @@ import org.bukkit.event.EventHandler;
 
 import com.arzio.arziolib.ArzioLib;
 import com.arzio.arziolib.api.event.packet.CDCombatlogShowTimer;
+import com.arzio.arziolib.api.util.CauldronUtils;
 import com.arzio.arziolib.module.ListenerModule;
 
-public class ModuleFixCorpseDuplication extends ListenerModule{
+import cpw.mods.fml.common.IPlayerTracker;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class ModuleFixCorpseDuplication extends ListenerModule implements IPlayerTracker{
 
 	public ModuleFixCorpseDuplication(ArzioLib plugin) {
 		super(plugin);
@@ -26,7 +31,11 @@ public class ModuleFixCorpseDuplication extends ListenerModule{
 	public void onEnable() {
 		super.onEnable();
 		
-		getPlugin().getCraftingDeadMain().setTrackerEnabled(false);
+		// We remove the CD tracker, and then put ours
+		this.getPlugin().getCraftingDeadMain().setTrackerEnabled(false);
+		if (!CauldronUtils.getForgePlayerTrackers().contains(this)) {
+			GameRegistry.registerPlayerTracker(this);
+		}
 	}
 
 	@Override
@@ -34,10 +43,42 @@ public class ModuleFixCorpseDuplication extends ListenerModule{
 		super.onDisable();
 		
 		// Only disable this module if the plugin IS NOT being disabled
+		// So we remove our tracker, and put back the CD's tracker
 		if (this.getPlugin().isEnabled()) {
+			CauldronUtils.getForgePlayerTrackers().remove(this);
 			getPlugin().getCraftingDeadMain().setTrackerEnabled(true);
 		}
 
 	}
+
+	@Override
+	public void onPlayerLogin(EntityPlayer player) {
+		
+		// We trick the onPlayerLogin of CD's Tracker by making the player's gamemode
+		// be CREATIVE only for this little amount of time.
+		
+		boolean isCreativeMode = player.abilities.canInstantlyBuild;
+		player.abilities.canInstantlyBuild = true;
+		this.getPlugin().getCraftingDeadMain().getPlayerTracker().onPlayerLogin(player);
+		player.abilities.canInstantlyBuild = isCreativeMode;
+	}
+
+	@Override
+	public void onPlayerLogout(EntityPlayer player) {
+		
+		// We trick the onPlayerLogin of CD's Tracker by making the player's gamemode
+		// be CREATIVE only for this little amount of time.
+		
+		boolean isCreativeMode = player.abilities.canInstantlyBuild;
+		player.abilities.canInstantlyBuild = true;
+		this.getPlugin().getCraftingDeadMain().getPlayerTracker().onPlayerLogout(player);
+		player.abilities.canInstantlyBuild = isCreativeMode;
+	}
+
+	@Override
+	public void onPlayerChangedDimension(EntityPlayer player) { }
+
+	@Override
+	public void onPlayerRespawn(EntityPlayer player) { }
 
 }
