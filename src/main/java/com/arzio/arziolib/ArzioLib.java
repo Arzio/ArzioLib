fc2b0cf7c640f96e75b36a33573955f2f53974a4
arzio <arziofox@gmail.com>
2019-03-05 00:55:14 -0300

    Added new module to auto-respawn in base

--- src/main/java/com/arzio/arziolib/ArzioLib.java
+++ src/main/java/com/arzio/arziolib/ArzioLib.java
@@ -37,6 +37,7 @@ import com.arzio.arziolib.module.addon.ModuleAddonInfinityEnchantCompatiblity;
 import com.arzio.arziolib.module.addon.ModuleAddonKeepEXPAfterDeath;
 import com.arzio.arziolib.module.addon.ModuleAddonProjectileProtectionCompatibility;
 import com.arzio.arziolib.module.addon.ModuleAddonRealSound;
+import com.arzio.arziolib.module.addon.ModuleAddonRespawnAtBase;
 import com.arzio.arziolib.module.addon.ModuleAddonStackableGrenades;
 import com.arzio.arziolib.module.addon.ModuleAddonStepEmeraldHeal;
 import com.arzio.arziolib.module.addon.ModuleAddonZombieFollowGrenades;
@@ -110,6 +111,7 @@ public class ArzioLib extends JavaPlugin {
 		this.moduleManager.registerModule(new ModuleAddonKeepEXPAfterDeath(this, false));
 		this.moduleManager.registerModule(new ModuleAddonProjectileProtectionCompatibility(this));
 		this.moduleManager.registerModule(new ModuleAddonRealSound(this));
+		this.moduleManager.registerModule(new ModuleAddonRespawnAtBase(this, false));
 		this.moduleManager.registerModule(new ModuleAddonStackableGrenades(this));
 		this.moduleManager.registerModule(new ModuleAddonStepEmeraldHeal(this, false));
 		this.moduleManager.registerModule(new ModuleAddonZombieFollowGrenades(this));
---
+++ src/main/java/com/arzio/arziolib/module/addon/ModuleAddonRespawnAtBase.java
@@ -0,0 +1,32 @@
+package com.arzio.arziolib.module.addon;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import com.arzio.arziolib.ArzioLib;
+import com.arzio.arziolib.api.bases.BaseProvider;
+import com.arzio.arziolib.module.ListenerModule;
+
+public class ModuleAddonRespawnAtBase extends ListenerModule {
+	
+	public ModuleAddonRespawnAtBase(ArzioLib plugin, boolean defaultState) {
+		super(plugin, defaultState);
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onRespawn(PlayerRespawnEvent event){
+		BaseProvider provider = this.getPlugin().getBaseProvider();
+		Player player = event.getPlayer();
+		
+		if (provider.hasBase(player)) {
+			player.teleport(provider.getBaseFromPlayer(player).getLocation());
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "addon-respawn-at-base";
+	}
+
+}
