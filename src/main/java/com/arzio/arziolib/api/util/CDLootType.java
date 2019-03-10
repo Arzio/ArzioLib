9d871658825a6c01088a910719ba0164f87d8fe5
arzio <arziofox@gmail.com>
2019-03-09 23:26:01 -0300

    Added material to loot types

--- src/main/java/com/arzio/arziolib/api/util/CDLootType.java
+++ src/main/java/com/arzio/arziolib/api/util/CDLootType.java
@@ -1,20 +1,27 @@
 package com.arzio.arziolib.api.util;
 
+import org.bukkit.block.Block;
+
 import com.arzio.arziolib.ArzioLib;
 import com.arzio.arziolib.api.wrapper.Loot;
 
 public enum CDLootType {
 
-	RESIDENTIAL("residential"),
-	RESIDENTIAL_RARE("residentialrare"),
-	MEDICAL("medical"),
-	MILITARY("military"),
-	POLICE("police");
+	RESIDENTIAL("residential", CDMaterial.RESIDENTIAL_LOOT),
+	RESIDENTIAL_RARE("residentialrare", CDMaterial.RARE_RESIDENTIAL_LOOT),
+	MEDICAL("medical", CDMaterial.MEDICAL_LOOT),
+	MILITARY("military", CDMaterial.MILITARY_LOOT),
+	POLICE("police", CDMaterial.POLICE_LOOT);
 	
 	private String id;
+	private CDMaterial material;
 	
-	private CDLootType(String id) {
+	private CDLootType(String id, CDMaterial material) {
 		this.id = id;
+	}
+	
+	public CDMaterial getMaterial() {
+		return this.material;
 	}
 	
 	public String getId() {
@@ -23,6 +30,15 @@ public enum CDLootType {
 	
 	public Loot getLoot() {
 		return ArzioLib.getInstance().getLootProvider().getLoot(this);
+	}
+	
+	public static CDLootType getFrom(Block block) {
+		for (CDLootType lootType : CDLootType.values()) {
+			if (block.getType() == lootType.getMaterial().asMaterial()) {
+				return lootType;
+			}
+		}
+		return null;
 	}
 
 }
