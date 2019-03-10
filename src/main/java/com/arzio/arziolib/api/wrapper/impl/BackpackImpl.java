645ae901b8534c6e9e9b84aaa540d55a5552a762
arzio <arziofox@gmail.com>
2019-03-10 13:09:54 -0300

    Added implementation for backpack wrapper

---
+++ src/main/java/com/arzio/arziolib/api/wrapper/impl/BackpackImpl.java
@@ -0,0 +1,13 @@
+package com.arzio.arziolib.api.wrapper.impl;
+
+import org.bukkit.Material;
+
+import com.arzio.arziolib.api.wrapper.Backpack;
+
+public class BackpackImpl extends CDItemImpl implements Backpack{
+
+	public BackpackImpl(Material material) {
+		super(material);
+	}
+
+}
