583c9b3c7dd7f3d0d8126e5936c4aae37764eeb9
arzio <arziofox@gmail.com>
2019-03-10 13:10:03 -0300

    Added implementation for item grenade wrapper

---
+++ src/main/java/com/arzio/arziolib/api/wrapper/impl/GrenadeImpl.java
@@ -0,0 +1,13 @@
+package com.arzio.arziolib.api.wrapper.impl;
+
+import org.bukkit.Material;
+
+import com.arzio.arziolib.api.wrapper.Grenade;
+
+public class GrenadeImpl extends CDItemImpl implements Grenade{
+
+	public GrenadeImpl(Material material) {
+		super(material);
+	}
+
+}
