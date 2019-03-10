e2be0dc46786e5adb176db5ddc19e48e7e07e1c1
arzio <arziofox@gmail.com>
2019-03-10 13:10:09 -0300

    Added implementation for hat wrapper

---
+++ src/main/java/com/arzio/arziolib/api/wrapper/impl/HatImpl.java
@@ -0,0 +1,13 @@
+package com.arzio.arziolib.api.wrapper.impl;
+
+import org.bukkit.Material;
+
+import com.arzio.arziolib.api.wrapper.Hat;
+
+public class HatImpl extends CDItemImpl implements Hat{
+
+	public HatImpl(Material material) {
+		super(material);
+	}
+
+}
