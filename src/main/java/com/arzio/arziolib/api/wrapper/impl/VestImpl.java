fb259dd98420d159b56d9d062be99aec3853b01a
arzio <arziofox@gmail.com>
2019-03-10 13:10:14 -0300

    Added implementation for vest wrapper

---
+++ src/main/java/com/arzio/arziolib/api/wrapper/impl/VestImpl.java
@@ -0,0 +1,13 @@
+package com.arzio.arziolib.api.wrapper.impl;
+
+import org.bukkit.Material;
+
+import com.arzio.arziolib.api.wrapper.Vest;
+
+public class VestImpl extends CDItemImpl implements Vest{
+
+	public VestImpl(Material material) {
+		super(material);
+	}
+
+}
