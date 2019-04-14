42af27734b31300b611ca69f934137e85ed1f598
arzio <arziofox@gmail.com>
2019-04-13 21:18:37 -0300

    Removed unused methods

--- src/main/java/com/arzio/arziolib/api/event/packet/CDGunReloadEvent.java
+++ src/main/java/com/arzio/arziolib/api/event/packet/CDGunReloadEvent.java
@@ -9,7 +9,6 @@ import com.arzio.arziolib.api.wrapper.Gun;
 public class CDGunReloadEvent extends GunEvent{
 
 	private static final HandlerList handlers = new HandlerList();
-	private boolean spendAmmo = true;
 
 	public CDGunReloadEvent(Player player, Gun gun, CDPacketDataWrapper dataWrapper) {
 		super(player, gun, dataWrapper);
@@ -18,14 +17,6 @@ public class CDGunReloadEvent extends GunEvent{
 	@Override
 	public HandlerList getHandlers() {
 		return handlers;
-	}
-	
-	public void setSpendAmmo(boolean willSpend) {
-		this.spendAmmo = willSpend;
-	}
-	
-	public boolean willSpendAmmo() {
-		return this.spendAmmo;
 	}
 	
 	public static HandlerList getHandlerList() {
