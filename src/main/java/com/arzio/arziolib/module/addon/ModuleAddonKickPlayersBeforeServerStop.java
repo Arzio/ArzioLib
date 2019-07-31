57253259538a0246f74292b8839d743a0328fcde
arzio <arziofox@gmail.com>
2019-07-30 23:54:44 -0300

    Remove a YML config and use Bukkit config instead

--- src/main/java/com/arzio/arziolib/module/addon/ModuleAddonKickPlayersBeforeServerStop.java
+++ src/main/java/com/arzio/arziolib/module/addon/ModuleAddonKickPlayersBeforeServerStop.java
@@ -7,41 +7,24 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.server.PluginDisableEvent;
-import com.arzio.arziolib.config.YMLFile;
+import org.spigotmc.SpigotConfig;
+
 import com.arzio.arziolib.module.Module;
 import com.arzio.arziolib.module.RegisterModule;
 
 @RegisterModule(name = "addon-kick-players-before-server-stop")
 public class ModuleAddonKickPlayersBeforeServerStop extends Module {
 
-	private YMLFile yml;
-	private String kickMessage;
-
-	@Override
-	public void onEnable() {
-        yml = new YMLFile(this.getPlugin(), "module_configuration/kick-players-before-server-stop.yml");
-		this.kickMessage = yml.getValueWithDefault("kick-message", "Server restart");
-		yml.save();
-	}
-
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
 	public void onPluginDisable(PluginDisableEvent event){
 		CraftServer craftServer = (CraftServer) Bukkit.getServer();
 		boolean isServerStopping = !craftServer.getServer().isRunning();
 
-		if (isServerStopping && Bukkit.getOnlinePlayers().length > 0){
+		if (isServerStopping){
 			for (Player player : Bukkit.getOnlinePlayers()){
-				player.kickPlayer(ChatColor.translateAlternateColorCodes('&', this.kickMessage));
+				player.kickPlayer(ChatColor.translateAlternateColorCodes('&', SpigotConfig.restartMessage));
 			}
 		}
-	}
-
-	public void setKickMessage(String kickMessage){
-		this.kickMessage = kickMessage;
-	}
-
-	public String getKickMessage(){
-		return this.kickMessage;
 	}
 
 }
